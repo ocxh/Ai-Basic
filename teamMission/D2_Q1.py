df0ee076f797e7d479b85c14ca7fe3a6aa55e1c4
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-25 15:56:41 +0900

    Day2 TeamMission Q1,Q2

---
+++ teamMission/D2_Q1.py
@@ -0,0 +1,21 @@
+class Score:
+    def __init__(self, mid, final):
+        self.__mid = mid
+        self.__final = final
+
+    @property
+    def save(self):
+        return self.__mid, self.__final
+
+    @save.setter
+    def save(self, s):
+        self.__mid += s[0]
+        self.__final += s[1]
+
+    def prt(self):
+        print((self.__mid+self.__final)/2)
+
+
+score = Score(50, 75)
+score.save = 10, 20
+score.prt()
